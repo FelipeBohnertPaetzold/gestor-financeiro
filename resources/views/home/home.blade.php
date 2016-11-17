9a14f3d89f57c96657ba0dfdf0bcaeef2fbec323
FelipeBohnertPaetzold <felipe.paetzold@gmail.com>
2016-11-17 16:24:20 -0200

    melhoria

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -6,8 +6,11 @@
         <li>DashBoard</li>
     </ol>
     <div class="container-fluid">
-        <h1 class="ls-title-intro ls-ico-dashboard">DashBoard</h1>
-
-
+        <h2 class="ls-title-intro">
+            <span style="font-size: 20px; color: #8c8c8c">
+                Você está vendo seu "Resumo Financeiro"!
+            </span>
+            <strong style="color: #8c8c8c; float: right"><i class="ls-ico-dashboard"></i> Dashboard</strong>
+        </h2>
     </div>
 @endsection
