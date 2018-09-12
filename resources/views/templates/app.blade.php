a2ee08043f20a2c0da28fbdbf4dfef6afc642e80
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2018-09-11 21:22:41 -0300

    test

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -95,7 +95,11 @@
     @yield('content')
 </main>
 <!-- We recommended use jQuery 1.10 or up -->
-<script src="/js/all.js"></script>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="http://assets.locaweb.com.br/locastyle/3.9.0/javascripts/locastyle.js" type="text/javascript"></script>
+<!-- Scripts -->
+<script src="/js/app.js"></script>
+<script src="/js/all.js"></script>
 </body>
 </html>
