c49a3dd2aa78bcc36cc8f6e6b091caa8795e0121
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2018-09-11 21:26:33 -0300

    formatmoney

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -94,6 +94,18 @@
 
     @yield('content')
 </main>
+<script>
+    Number.prototype.formatMoney = function (c, d, t) {
+        var n = this,
+            c = isNaN(c = Math.abs(c)) ? 2 : c,
+            d = d == undefined ? "." : d,
+            t = t == undefined ? "," : t,
+            s = n < 0 ? "-" : "",
+            i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+            j = (j = i.length) > 3 ? j % 3 : 0;
+        return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+    };
+</script>
 <!-- We recommended use jQuery 1.10 or up -->
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
